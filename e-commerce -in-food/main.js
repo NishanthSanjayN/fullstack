0c735c7da6da3a1f06f7a5780c5aba1300b59a6b
Nishanthxoxo <nishanthsanjay16@gmail.com>
2025-03-23 19:38:28 +0530

    Modified code

--- e-commerce -in-food/main.js
+++ e-commerce -in-food/main.js
@@ -118,6 +118,38 @@ $(document).ready(function ($) {
 
 });
 
+let searchForm=document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick=()=>{
+    searchForm.classList.toggle('active');
+}
+
+
+document.getElementById("login-btn").addEventListener("click", function() {
+    window.location.href = "login.html";  
+});
+
+let navbar=document.querySelector('.navbar');
+    document.querySelector('#menu-btn').onclick=()=>{
+        navbar.classList.toggle('active');
+    }
+window.onscroll=()=>{
+    searchForm.classList.remove('active');
+}
+var swiper = new Swiper(".book-table-img-slider", {
+    slidesPerView: 3,  // Number of slides visible at a time
+    spaceBetween: 30,  // Space between slides
+    loop: true,        // Infinite loop
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
 
 jQuery(window).on('load', function () {
     $('body').removeClass('body-fixed');
--- e-commerce -in-food/script.js
+++ e-commerce -in-food/script.js
@@ -2,3 +2,30 @@ let searchForm=document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick=()=>{
     searchForm.classList.toggle('active');
 }
+
+
+document.getElementById("login-btn").addEventListener("click", function() {
+    window.location.href = "login.html";  
+});
+
+let navbar=document.querySelector('.navbar');
+    document.querySelector('#menu-btn').onclick=()=>{
+        navbar.classList.toggle('active');
+    }
+window.onscroll=()=>{
+    searchForm.classList.remove('active');
+}
+var swiper = new Swiper(".book-table-img-slider", {
+    slidesPerView: 3,  // Number of slides visible at a time
+    spaceBetween: 30,  // Space between slides
+    loop: true,        // Infinite loop
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
